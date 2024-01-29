f264eac19efe0545ab578a1d9e572598da563763
Oleksandr <gayrsuk@gmail.com>
2024-01-29 21:58:25 +0200

    refactored styles. added scripts for opened and closed modals

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 const hamburgerBtn = document.getElementById('mobile-menu-open');
 const mobileMenu = document.getElementById('mobile-menu');
 const closedMobileMenuBtn = document.getElementById('close-mobile-menu');
+const orderBtn = document.getElementById('order-btn');
+const modalForm = document.getElementById('modal-form');
+const closeModalBtn = document.getElementById('close-modal-btn');
 
 
 hamburgerBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('is-modal-open')
+    mobileMenu.classList.add('is-modal-open');
 });
 
 
 closedMobileMenuBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('is-modal-open')
+    mobileMenu.classList.remove('is-modal-open');
 });
+
+orderBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    modalForm.classList.add('is-modal-open');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modalForm.classList.remove('is-modal-open');
+})
